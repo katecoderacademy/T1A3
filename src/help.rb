2e7f81f3b0afb577331e2a8c4d5480f5ee26874f
katecoderacademy <cam022111@coderacademy.edu.au>
2021-09-27 02:59:06 +1000

    Creating the basic files, defining first classes ie party, year, starting data entry

---
+++ src/help.rb
@@ -0,0 +1,13 @@
+help = 
+{1: "Applications can be brought by one party to the court in certain circumstances. /n
+    Where one party brings the application, the application will be 'Ex parte' /n
+    Where a matter is 'in the matter of' and generally in relation to guardianship  /n
+    matters where the court is acting in an advisory capacity. This is shortened to 'Re'./n
+    When citing a relator action, i.e. brought by a third party as the first party lacks /n
+    legal capacity to sue in their own name, the 'relator' action will be cited as 'ex rel' /n"
+    #Ex Parte, ex rel, re
+2:  "When suing a vessel, only the name of the vessel should appear. If the suit is in /n
+    personem, then the vessel is appended after the parties names."
+    #Suing a ship
+3:  
+}
---
+++ src/xcitation.rb
@@ -0,0 +1,17 @@
+require party
+require law-report
+require help
+
+citition = []
+user_selections = [exparte, party_1, party_2, party_3 ship, law-report]
+program_data = []
+
+class year
+    def initialize
+        @year_date = year_date
+    end
+end
+
+
+
+#ignore null entries
