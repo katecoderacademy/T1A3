3df3033390575eed2693fbd01d00da90863daba5
katecoderacademy <cam022111@coderacademy.edu.au>
2021-10-02 03:59:33 +1000

    appending further help information to the help file.

--- src/help.rb
+++ src/help.rb
@@ -13,7 +13,24 @@ two:  "When suing a vessel, only the name of the vessel should appear. If the su
 three:  "When citing a relator action, i.e. brought by a third party as the first party lacks 
 legal capacity to sue in their own name, the 'relator' action will be cited as 'ex rel'."
     #Suing a ship
-    
+three:  "Structures of Corporations is dictated by the Corporations Act 2001. The relevant 
+        structures are \n 
+        Proprietary Limited - The most common type of structure of a private company 
+        [limited in the way it can seek public investment] where its investors are limited 
+        in their liability to the company only in the amount of unpaid shares they posess.\n
+        Limited - a public company. Public increases reporting obligations regarding its 
+        finances, but also allows it to raise share capital from ordinary investors on the 
+        share market. Investors to a limited company are also limited in their liability to
+        the company to the value of their unpaid shares.\n
+        Proprietary - an unlimited in liability but privately held company. Very uncommon.\n
+        Incorporated - not specifically company structures but an entity entitled to an 
+        ABN. May consist of other structures such as a body corporate, an incorporated association,
+        or more frequently a foreign company [as 'inc' is an American suffix].\n  
+        No Liability - a company type only available to certain types of mining ventures.
+        Investors who commit to buying shares are never required to pay for them. Was 
+        invented to allow for companies who do speculative mining exploration and to make it
+        more appealing to potential investors to invest."
+        #Suing a ship
 }
 
 
